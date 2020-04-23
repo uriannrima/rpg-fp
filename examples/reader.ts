e813c2c78509f381ecb02cb3c3eebfa148ee16b7
Luciano Lima <luscious.ds@gmail.com>
2020-04-23 23:07:37 +0100

    feat(reader): More examples

--- examples/reader.ts
+++ examples/reader.ts
@@ -1,58 +1,60 @@
-import * as R from "fp-ts/lib/Reader"
-import * as E from "fp-ts/lib/Either"
+import * as R from "fp-ts/lib/Reader";
+import * as E from "fp-ts/lib/Either";
 import { pipe } from "fp-ts/lib/pipeable";
-import * as Ramda from 'ramda';
+import * as Ramda from "ramda";
 import { flow } from "fp-ts/lib/function";
 
-const f = (b: boolean) => (b ? 'true' : 'false')
+const f = (b: boolean) => (b ? "true" : "false");
 const g = (n: number) => f(n > 2);
 const h = (s: string) => g(s.length + 1);
 
-console.log(h('foo'));
+console.log(h("foo"));
 
 /** Required Dependencies */
 
 /** i18n*/
 interface Dependencies {
-    i18n: {
-        true: string;
-        false: string;
-    };
+  i18n: {
+    true: string;
+    false: string;
+  };
 
-    lowerBound: number;
+  lowerBound: number;
 }
 
 const instance: Dependencies = {
-    i18n: {
-        false: 'falso',
-        true: 'verdadeiro'
-    },
-    lowerBound: 4,
-}
+  i18n: {
+    false: "falso",
+    true: "verdadeiro",
+  },
+  lowerBound: 4,
+};
 
 /** We must change signature (add deps to all chain) and change the code! */
-const f2 = (b: boolean, deps: Dependencies): string => (b ? deps.i18n.true : deps.i18n.false);
+const f2 = (b: boolean, deps: Dependencies): string =>
+  b ? deps.i18n.true : deps.i18n.false;
 const g2 = (n: number, deps: Dependencies): string => f2(n > 2, deps);
 const h2 = (s: string, deps: Dependencies): string => g2(s.length + 1, deps);
-console.log(h2('foo', instance));
+console.log(h2("foo", instance));
 
 /** Dependency as return type... */
 
-/** 
+/**
  * Even though we have changed the SIGNATURE of the return value of "F" (and because of it, propagated to G and H),
  * We didn't have to make any change to G and H, no need to change nothing on the code.
  * We may explicity tell the return, but we aren't obliged to (see g3 and h3).
  */
-const f3 = (b: boolean) => (deps: Dependencies): string => (b ? deps.i18n.true : deps.i18n.false);
+const f3 = (b: boolean) => (deps: Dependencies): string =>
+  b ? deps.i18n.true : deps.i18n.false;
 const g3 = (n: number) => f3(n > 2);
-const h3 = (s: string): (deps: Dependencies) => string => g3(s.length + 1);
+const h3 = (s: string): ((deps: Dependencies) => string) => g3(s.length + 1);
 
 /** Now, almost everything is the same, but we need to pass the dependencies */
-h3('foo')(instance);
+h3("foo")(instance);
 
 /** Reader<D, A> => (dependency: D) => A */
 
-/** 
+/**
  * F3 is of type (dep: A) => B
  * Any "(a: A) => B" where A is a dependency, we can use Reader<A, B>
  * Since F uses Dependencies, to return a string, we say that
@@ -60,11 +62,12 @@ h3('foo')(instance);
  * Reader<Dependencies,String> => (a: Dependencies) => string;
  */
 
-const f4 = (b: boolean): R.Reader<Dependencies, string> => (deps): string => (b ? deps.i18n.true : deps.i18n.false);
+const f4 = (b: boolean): R.Reader<Dependencies, string> => (deps): string =>
+  b ? deps.i18n.true : deps.i18n.false;
 const g4 = (n: number): R.Reader<Dependencies, string> => f4(n > 2);
 const h4 = (s: string): R.Reader<Dependencies, string> => g4(s.length + 1);
 
-h4('foo')(instance)
+h4("foo")(instance);
 
 /**
  * We now, can inject in between the line...
@@ -74,79 +77,141 @@ h4('foo')(instance)
  * R.map -> Consume, and thats it, the return becomes the product to the other
  * R.chain -> Consume the Reader of A, and the return B becomes Reader<A, B>
  */
-const g5 = (n: number): R.Reader<Dependencies, string> => pipe(
+const g5 = (n: number): R.Reader<Dependencies, string> =>
+  pipe(
     /** Ask for Deps for the next function which will use it */
     R.ask<Dependencies>(),
     /** Chain the function to use the dependecy asked */
-    R.chain(deps => f4(n > deps.lowerBound))
-)
+    R.chain((deps) => f4(n > deps.lowerBound))
+  );
 
 const h5 = (s: string) => g5(s.length + 1);
 
-h5('foo')({ ...instance, lowerBound: 10 }); // False
+h5("foo")({ ...instance, lowerBound: 10 }); // False
+
+/**
+ * Inside our domain now...
+ */
 
 enum AbilityScoreType {
-    Intelligence = 'Intelligence',
-    Charisma = 'Charisma'
+  Intelligence = "Intelligence",
+  Charisma = "Charisma",
 }
 
 type AbilityScoreName = string | AbilityScoreType;
 
 type AbilityScore = {
-    name: AbilityScoreName;
-    value: number;
-}
+  name: AbilityScoreName;
+  value: number;
+};
 
-type AbilityScores = Array<AbilityScore>
+type AbilityScores = Array<AbilityScore>;
 
 enum SkillType {
-    Appraise = 'Appraise',
-    Bluff = 'Bluff'
+  Appraise = "Appraise",
+  Bluff = "Bluff",
 }
 
 type SkillName = string | SkillType;
 
 type Skill = {
-    name: SkillName;
-    keyScore: AbilityScoreName;
-}
+  name: SkillName;
+  keyScore: AbilityScoreName;
+};
 
 type Skills = Skill[];
 
 const abilityScores: AbilityScores = [
-    { name: AbilityScoreType.Intelligence, value: 12 }
+  { name: AbilityScoreType.Intelligence, value: 12 },
 ];
 
-const getAbilityScore = (scoreName: AbilityScoreName): E.Either<Error, AbilityScore> => {
-    const score = Ramda.find<AbilityScore>(score => score.name == scoreName)(abilityScores);
-    return E.fromNullable(new Error(`Score "${scoreName}" not found.`))(score);
-}
+const getAbilityScore = (
+  scoreName: AbilityScoreName
+): E.Either<Error, AbilityScore> => {
+  const score = Ramda.find<AbilityScore>((score) => score.name == scoreName)(
+    abilityScores
+  );
+  return E.fromNullable(new Error(`Score "${scoreName}" not found.`))(score);
+};
+
+const getAbilityScore2 = (
+  scoreName: AbilityScoreName
+): R.Reader<Enviroment, E.Either<Error, AbilityScore>> => (
+  environment: Enviroment
+) => {
+  const score = Ramda.find<AbilityScore>((score) => score.name == scoreName)(
+    environment.abilityScores
+  );
+  return E.fromNullable(new Error(`Score "${scoreName}" not found.`))(score);
+};
 
 const skills: Skills = [
-    { name: SkillType.Appraise, keyScore: AbilityScoreType.Intelligence },
-    { name: SkillType.Bluff, keyScore: AbilityScoreType.Charisma },
+  { name: SkillType.Appraise, keyScore: AbilityScoreType.Intelligence },
+  { name: SkillType.Bluff, keyScore: AbilityScoreType.Charisma },
 ];
 
 const getKeyScore = ({ keyScore }: { keyScore: AbilityScoreName }) => keyScore;
 
 const getSkill = (skillName: SkillName): E.Either<Error, Skill> => {
-    const skill = Ramda.find<Skill>(skill => skill.name == skillName)(skills);
-    return E.fromNullable(new Error(`Skill "${skillName}" not found.`))(skill);
+  const skill = Ramda.find<Skill>((skill) => skill.name === skillName)(skills);
+  return E.fromNullable(new Error(`Skill "${skillName}" not found.`))(skill);
+};
+
+interface Enviroment {
+  skills: Skills;
+  abilityScores: AbilityScores;
 }
 
+/**
+ * Now we have an enviroment variable injected in our getSkill
+ * Which is used to get the skills without dealing with the "global" definition...
+ */
+const getSkillWithReader = (
+  skillName: SkillName
+): R.Reader<Enviroment, E.Either<Error, Skill>> => (
+  enviroment
+): E.Either<Error, Skill> => {
+  const skill = Ramda.find<Skill>((skill) => skill.name === skillName)(
+    enviroment.skills
+  );
+  return E.fromNullable(new Error(`Skill "${skillName}" not found.`))(skill);
+};
+
 const getSkillAbilityScore = flow(
-    getSkill,
-    E.map(getKeyScore),
-    // If we map again, we'll have an Either<Error, Either<Error, AbilityScore>>
-    // To avoid it, we use chain, or flatMap, since both has the same left as Error
-    // And we end up with a Either<Error,AbilityScore>
-    E.chain(getAbilityScore)
+  getSkill,
+  E.map(getKeyScore),
+  // If we map again, we'll have an Either<Error, Either<Error, AbilityScore>>
+  // To avoid it, we use chain, or flatMap, since both has the same left as Error
+  // And we end up with a Either<Error,AbilityScore>
+  E.chain(getAbilityScore)
 );
 
-const as1 = getSkillAbilityScore(SkillType.Appraise);
-const as2 = getSkillAbilityScore('No Skill'); // Skill "No Skill" not found.
-const as3 = getSkillAbilityScore(SkillType.Bluff); // Score "Charisma" not found.
-
-console.log(
-    { as1, as2, as3 }
+/**
+ * Now we may inject the Enviroment at the end
+ * We need to map using Reader
+ * And keep maing Either
+ */
+const getSkillAbilityScore2 = flow(
+  getSkillWithReader,
+  R.map(flow(E.map(getKeyScore), E.chain(getAbilityScore2)))
 );
+
+const as1 = getSkillAbilityScore(SkillType.Appraise); // Right({ name: Intelligence, value:12 })
+const as2 = getSkillAbilityScore("No Skill"); // Left(Skill "No Skill" not found.)
+const as3 = getSkillAbilityScore(SkillType.Bluff); // Left(Score "Charisma" not found.)
+
+const eInstance: Enviroment = {
+  abilityScores: [
+    ...abilityScores,
+    { name: AbilityScoreType.Charisma, value: 12 },
+  ],
+  skills: [
+    ...skills,
+    { keyScore: AbilityScoreType.Intelligence, name: "No Skill" },
+  ],
+};
+
+const as4 = getSkillAbilityScore2("No Skill")(eInstance); // Right({ name: Intelligence, value:12 })
+const as5 = getSkillAbilityScore2(SkillType.Bluff)(eInstance); // Right({ name: Intelligence, value:12 })
+
+console.log({ as1, as2, as3, as4, as5 });
